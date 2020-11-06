f29fe6575209d8a0e04bb42cdaa7e6fb44c1916b
szaghdoudi <sabri.zaghdoudi@pc-scol.fr>
2020-11-06 10:27:51 +0100

    dead code

--- printer-server/src/test/java/fr/pcscol/printer/service/freemarker/FreemarkerPrinterServiceTest.java
+++ printer-server/src/test/java/fr/pcscol/printer/service/freemarker/FreemarkerPrinterServiceTest.java
@@ -2,8 +2,6 @@ package fr.pcscol.printer.service.freemarker;
 
 import com.google.common.collect.Lists;
 import fr.pcscol.printer.PvJury;
-import freemarker.template.Configuration;
-import freemarker.template.TemplateExceptionHandler;
 import org.apache.catalina.webresources.TomcatURLStreamHandlerFactory;
 import org.junit.Assert;
 import org.junit.Test;
@@ -67,37 +65,5 @@ public class FreemarkerPrinterServiceTest {
         } catch (Exception e) {
             Assert.fail(e.getMessage());
         }
-    }
-
-
-    public Configuration getConfig() throws IOException {
-        // Create your Configuration instance, and specify if up to what FreeMarker
-// version (here 2.3.29) do you want to apply the fixes that are not 100%
-// backward-compatible. See the Configuration JavaDoc for details.
-        Configuration cfg = new Configuration(Configuration.VERSION_2_3_29);
-
-// Specify the source where the template files come from. Here I set a
-// plain directory for it, but non-file-system sources are possible too:
-        cfg.setDirectoryForTemplateLoading(new File("./src/test/resources/freemarker"));
-
-// From here we will set the settings recommended for new projects. These
-// aren't the defaults for backward compatibilty.
-
-// Set the preferred charset template files are stored in. UTF-8 is
-// a good choice in most applications:
-        cfg.setDefaultEncoding("UTF-8");
-
-// Sets how errors will appear.
-// During web page *development* TemplateExceptionHandler.HTML_DEBUG_HANDLER is better.
-        cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
-
-
-// Wrap unchecked exceptions thrown during template processing into TemplateException-s:
-        cfg.setWrapUncheckedExceptions(true);
-
-// Do not fall back to higher scopes when reading a null loop variable:
-        cfg.setFallbackOnNullLoopVariable(false);
-
-        return cfg;
     }
 }
